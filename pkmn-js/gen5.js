52455817ee6dc05314c9fe03ad6620e634b4718c
Raymond Dodge <git@rayrobdod.name>
2014-12-05 16:47:53 -0500

    Adding a  Lilly, Cover and 0000M to the park. Adding info on Jimmy.

--- pkmn-js/gen5.js
+++ pkmn-js/gen5.js
@@ -123,64 +123,144 @@ addEvent(new Pokemon({
 }));
 
 
+// TODO: Bird Moses
+
+addEvent(new Pokemon({
+	name : "Lilly",
+	sprite: "img/pkmn/lilly.png",
+	x: 32, y: 28,
+	
+	dex : "img/pkdx/bldex_lilly.png",
+	sources : {
+		"Sprite by /u/carlotta4th" : "http://www.reddit.com/r/twitchplayspokemon/comments/252a2b/tpp_sprites/",
+		"Pokedex Image by /u/carlotta4th" : "http://www.reddit.com/r/twitchplayspokemon/comments/252a2b/tpp_sprites/"
+	},
+	
+	OT: "GMYC&nbsp;&nbsp;",
+	gender: 2,
+	gamename : "Lillil",
+	pokename : "Petilil",
+	nicknames : "Princess Lilly",
+	level : 41,
+	caught : "Recieved in a trade: 1d 17h 37m",
+	ball: "poke",
+	memo : "Avid musical participant. Usually seen with red specs.",
+	
+	ribbons : [
+		new Traded_Ribbon("For Cottonee 1d 17h 37m"),
+		new Released_Ribbon("5d 6h 36m"),
+	],
+}));
+addEvent(new Pokemon({
+	name : "Lord Cover",
+	sprite: "img/pkmn/cover.png",
+	x: 30, y: 28,
+	adj_flip: true,
+	
+	dex : "img/pkdx/bldex_cover.png",
+	sources : {
+		"Sprite by /u/carlotta4th" : "http://www.reddit.com/r/twitchplayspokemon/comments/252a2b/tpp_sprites/",
+		"Pokedex Image by /u/carlotta4th" : "http://www.reddit.com/r/twitchplayspokemon/comments/252a2b/tpp_sprites/"
+	},
+	
+	OT: "GMYC&nbsp;&nbsp;",
+	gender: 1,
+	gamename : "Tirtouga",
+	pokename : "Tirtouga",
+	nicknames : "",
+	level : 36,
+	caught : "Revived from fossil: 3d 6h 18m",
+	ball: "poke",
+	memo : "Avid musical participant. Usually seen wearing a pink bow.",
+	
+	ribbons : [
+		new Released_Ribbon("5d 6h 38m"),
+	],
+}));
+
+
+addEvent(new Pokemon({
+	name : "0000M",
+	sprite: "img/pkmn/0000m.png",
+	x: -18, y: -30,
+	adj_flip: true,
+	
+	dex : "img/pkdx/b/Spr_5b_643.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "GMYC&nbsp;&nbsp;",
+	gender: null,
+	gamename : "0000M",
+	pokename : "Reshiram",
+	nicknames : "",
+	level : 50,
+	caught : "during the 9th battle: 12d 18h 0m",
+	ball: "dusk",
+	memo : "Was excited to join Jimmy's fight against N, but the Truth that Jimmy fought for required her to stay at the sidelines.",
+	
+	ribbons : [
+	],
+}));
+
+
+// TODO: The six hall of famers
 
 
 // Jimmy!!
 addEvent(new Trainer({
-	skipme: true,
 	name : "GMYC&nbsp;&nbsp;",
 	spritesheet : "img/trainers/napoleon.png",
-	x: -9, y: 13,
+	x: -15, y: 20,
 	
-	dex : "img/pkdx/b/Spr_Pt_Lucas.png",
+	dex : "img/pkdx/trainer_jimmy.png",
 	sources : {
-		"Lucas Sprite from Bulbapedia":"", 
+		"Pokedex Image by /u/carlotta4th" : "http://www.reddit.com/r/twitchplayspokemon/comments/252a2b/tpp_sprites/"
 	},
 	
-	idnum : "12339",
-	nickname : "Napoleon",
-	altnicks : "Nipple",
-	playtime: "17d 11h 39m",
-	pokedex : "52 own/206 seen",
-	releasecount : "4 (+22)",
-	catchcount : 0,
-	e4attempts : 49,
-	blackouts : 173,
+	idnum : "12356",
+	nickname : "Jimmy C.",
+	altnicks : "Gimmick",
+	playtime: "12d 18h 21m",
+	pokedex : "39 own/135 seen",
+	releasecount : 12,
+	catchcount : 121,
+	e4attempts : 62,
+	blackouts : "164+",
 	
-	personality: "Napoleon hates pokemon, or he did at the start of his adventure. He is a quiet, reserved 'dandy', more concerned with keeping himself looking fabulous than any adventure at hand. He's also a habitual gambler.",
+	personality: "Jimmy is an avid fan of the voices, and initially far too willing to obey their commands at the expense of his Pokémon. Has been known to play video games professionally.",
 	notable: 
-		"Number of Wooper Caught: 20<br/>"+
-		"Number of Pokeballs Bought: 400+<br/>"+
-		"Number of Times Time Traveled: 2",
+		"Number of Pokémon evolved: 0<br/>"+
+		"Fresh water bought: 53+"+
+		"",
 	ribbons: [
+		new Record_Ribbon("Least evolved Hall of Fame team"),
 	],
 	
-	// info_html : 
-	// 	"Number of E4 Attempts: 49<br/>"+
-	// 	"Times Blacked Out: 173<br/>"+
-	// 	"Number of Wooper Caught: 20<br/>"+
-	// 	"Number of Times Time Traveled: 2<br/>"+
-	// 	"Number of Pokeballs Bought: 400+",
 	icons : [
-		"img/icn/coin_case.png",
-		"img/icn/air_mail.png",
-		"img/icn/poke_ball.png",
-		"img/icn/shiny_stone.png",
-		"img/icn/contest_pass.png",
-		"img/icn/sun_stone.png",
+		// new Icon("contest_pass.png", "Where A failed, Napoleon succeeded: he participated, dressed formally in suit and tie, in several Contests. He didn't win any, however."),
 	],
 	
-	badge_html : "",
-	
-	behavior: behavior.look,
-	
-	dialog: [ 
-		"YOU SPELLED MY NAME WRONG!!",
-		"HELLO?! Anyone want to FIX MY NAME ON THIS SIGN?!?!",
-		"It's <em>NA</em>poleon! NAH! As in you can DO NOTHING TO MAKE ME HAPPY ABOUT THIS!!",
-		"I pay GOOD MONEY at the Game Corner and they CAN'T EVEN SPELL MY NAME RIGHT?!",
+	badges_info : {
+		img: "img/icn/badges_black.png",
+		frame_width: 16,
+		frame_height: 16,
+	},
+	badges : [
+		new Badge({ name: "Trio Badge", leader: "Cress", type: "Water" }),
+		new Badge({ name: "Basic Badge", leader: "Lenora", type: "Grass" }),
+		new Badge({ name: "Insect Badge", leader: "Burgh", type: "Bug" }),
+		new Badge({ name: "Bolt Badge", leader: "Elesa", type: "Electric" }),
+		new Badge({ name: "Quake Badge", leader: "Clay", type: "Ground" }),
+		new Badge({ name: "Jet Badge", leader: "Skyla", type: "Flying" }),
+		new Badge({ name: "Freeze Badge", leader: "Brycen", type: "Ice" }),
+		new Badge({ name: "Legend Badge", leader: "Drayden", type: "Dragon" }),
 	],
-	dialog_assignment: "random",
+	
+	behavior: behavior.meander,
+	
 	
 	// doClick : function(){
 	// 	Person.fn.doClick.call(this);
