d49860ae45d762b72dc3af7cc3f701db36f40064
Raymond Dodge <git@rayrobdod.name>
2014-12-07 04:53:44 -0500

    Adding data for: Scout, PVG, Wilbur, Quaffle, Zenny, Aepom jr., Umbrellagator, Zoro. Adding data to CLY.

--- pkmn-js/gen5.1.js
+++ pkmn-js/gen5.1.js
@@ -7,10 +7,226 @@
 // showGameClock("TPP Blaze Black 2 Game Clock<br/>", new Date(Date.UTC(1986, 07-1, 6, 0+4)));
 
 
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Scout",
+	sprite: "img/pkmn/.png",
+	x: -14, y: 20,
+	
+	dex : "img/pkdx/b2dex_scout.png",
+	sources : {
+		"Pokedex Image by /u/Pioxys" : "http://www.reddit.com/r/twitchplayspokemon/comments/2oisme/sprites_sheet_asktwitchpkmn8_main_cast_members/",
+	},
+	
+	OT: "Cly",
+	gender: 2,
+	gamename : "Sentret",
+	pokename : "Sentret",
+	nicknames : "",
+	level : 11,
+	caught: "0d 0h 46m: in Route 19 at level 5",
+	ball : "poke",
+	memo : "",
+	
+	ribbons : [
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Top Percentage",
+	sprite: "img/pkmn/.png",
+	x: -15, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 1,
+	gamename : ".'&nbsp;%&nbsp;@&nbsp;-&nbsp;PVG",
+	pokename : "Flaffy",
+	nicknames : "",
+	level : 21,
+	caught: "0d 1h 33m: in Floccesy Ranch at level 8",
+	ball : "poke",
+	memo : "Accidentally deposited while trying to enter a Pokéstar Studio movie.",
+	
+	ribbons : [
+	],
+}));
+
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Wilbur",
+	sprite: "img/pkmn/.png",
+	x: -15, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 1,
+	gamename : "VVUU",
+	pokename : "Emboar",
+	nicknames : "",
+	level : 100,
+	caught: "0d 0h 16m: Recieved from Bianca",
+	ball : "poke",
+	memo : "",
+	
+	ribbons : [
+		new Starter_Ribbon(),
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Quaffle",
+	sprite: "img/pkmn/.png",
+	x: -16, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 2,
+	gamename : "qfwwqhhzwu",
+	pokename : "Unfezant",
+	nicknames : "",
+	level : 100,
+	caught: "",
+	ball : "poke",
+	memo : "",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Zenny",
+	sprite: "img/pkmn/.png",
+	x: 2, y: -40,
+	
+	dex : "img/pkdx/b2dex_zenny.png",
+	sources : {
+		"Pokedex Image by /u/Pioxys" : "http://www.reddit.com/r/twitchplayspokemon/comments/2oisme/sprites_sheet_asktwitchpkmn8_main_cast_members/",
+	},
+	
+	OT: "Cly",
+	gender: 2,
+	gamename : "zzffzz&nbsp;zzw",
+	pokename : "Leavany",
+	nicknames : "",
+	level : 100,
+	caught: "0d 7h 27m: In Route 20 at level 6",
+	ball : "poke",
+	memo : "Cly's agent.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Aepom Jr.",
+	sprite: "img/pkmn/.png",
+	x: -18, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 1,
+	gamename : "AEEEEOPM.J",
+	pokename : "Sceptile",
+	nicknames : "President",
+	level : 99,
+	caught: "0d 19h 37m: Recieved from a harlequin",
+	ball : "poke",
+	memo : "The grinding at the end of CLY's time failed to make Aepom Lv.100. It swept Red's team regardless.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Umbrellagator",
+	sprite: "img/pkmn/.png",
+	x: -19, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 1,
+	gamename : "u2E!☂☂☂M☂'",
+	pokename : "Feraligatr",
+	nicknames : "Termigator, Sobek",
+	level : 100,
+	caught: "1d 21h 4m: Recieved from a harlequin",
+	ball : "poke",
+	memo : "Both alternate nicknames were used before this mon gained its current nickname from the name rater.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+addEvent(new Pokemon({
+	skipme: true,
+	name : "Zoro",
+	sprite: "img/pkmn/.png",
+	x: -20, y: 20,
+	
+	dex : "img/pkdx/b/.png",
+	sources : {
+		"Sprite from Bulbapedia" : "",
+		"Pokedex Image from Bulbapedia" : "",
+	},
+	
+	OT: "Cly",
+	gender: 1,
+	gamename : "Zoroark",
+	pokename : "Zoroark",
+	nicknames : "",
+	level : 100,
+	caught: "4d 2h 53m: Recieved from Rood",
+	ball : "poke",
+	memo : "Orignially N's Zorua. It's Illusion ability repeatedly confused the live updaters.",
+	
+	ribbons : [
+		new HallOfFame_Ribbon("16d 5h 34m"),
+	],
+}));
+
+
 // CLY!!
 addEvent(new Trainer({
 	skipme: true,
-	name : "CLY",
+	name : "CL Y .,",
 	spritesheet : "img/trainers/napoleon.png",
 	x: -15, y: 20,
 	
@@ -25,22 +241,23 @@ addEvent(new Trainer({
 	playtime: "19d 2h 17m",
 	pokedex : "",
 	releasecount : 0,
-	catchcount : "",
+	catchcount : 36,
 	e4attempts : 67,
 	blackouts : "",
 	
-	personality: "",
+	personality: "A quite dedicated trainer. A former singing idol, but seems to hve given up the life, especially as she only participated in the one Pokéstar Studios movie reuired for story progress.",
 	notable: 
-		"<br/>"+
+		"Final team average level: 99.8333333<br/>"+
 		""+
 		"",
 	ribbons: [
-		new Record_Ribbon("Highest average level on final team: 99.8333"),
+		new Record_Ribbon("Highest average level on final team"),
 		new Record_Ribbon("Lowest Release Count"),
 	],
 	
 	icons : [
 		// new Icon("", "forced evolutions? "),
+		new Icon("dusk_ball.png", "Went on a legendary-catching spree after beating the elite four: Four in dusk balls, two in master balls and one in a timer ball."),
 	],
 	
 	badges_info : {
